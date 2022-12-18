23e15400a0d1c958b75e72154820c12125403f99
LosBandito <91223061+LosBandito@users.noreply.github.com>
2022-12-18 20:46:35 +0100

    2 files modified

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -28,25 +28,25 @@ export class DetailComponent implements OnInit {
   }
 
   ngOnInit() {}
-
   onSubmit() {
-   
+    // Get the list of students from local storage
     let studentsString = localStorage.getItem('students');
     let students = JSON.parse(studentsString);
 
-   
-    let index = students.findIndex(
+    // Find the student object that we want to edit
+    let student = students.find(
       (student) =>
         student.name === this.students.name &&
         student.surname === this.students.surname
     );
 
-  
-    let now = new Date();
-    this.students.lastEdited = now;
-    students[index] = this.students;
+    console.log(this.students.name);
+    // Update the name and surname of the student using the values from the form input fields
+    student.name = this.students.name;
+    student.surname = this.students.surname;
+    student.class = this.students.classs;
 
-    
+    // Store the updated list of students back to local storage
     localStorage.setItem('students', JSON.stringify(students));
   }
 }
